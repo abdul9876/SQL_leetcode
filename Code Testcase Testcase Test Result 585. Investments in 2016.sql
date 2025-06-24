19e43289542a21555582d78175c6b482c5afa690
Abdul Muthallib <abdulmuthallib984@gmail.com>
2025-06-25 00:19:05 +0530

    subquerry

---
+++ Code Testcase Testcase Test Result 585. Investments in 2016.sql
@@ -0,0 +1,2 @@
+select ROUND(sum(tiv_2016),2) as tiv_2016 from Insurance 
+where tiv_2015 in (select tiv_2015 from Insurance Group by tiv_2015 having count(*)>1) and (lat,lon) in(select lat,lon from Insurance Group by lat,lon having count(*)=1);
